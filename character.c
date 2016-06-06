f021b5a2a7b5ff808940504b7082b934f2056271
sowmiyagaje <sowmiyagaje@users.noreply.github.com>
2016-06-06 11:32:30 +0530

    Create character.c

---
+++ character.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	char s;
+	scanf("%c",&s);
+	if(s=='a'&&s=='e'&&s=='i'&&s=='o'&&s=='u')
+	printf("vowel");
+	else
+	printf("consonant");
+	return 0;
+}
