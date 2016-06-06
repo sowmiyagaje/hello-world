4e5e616822eee849d15406ab14e07426d21ec29c
sowmiyagaje <sowmiyagaje@users.noreply.github.com>
2016-06-06 14:00:05 +0530

    Create alphabet.c

---
+++ include-stdio.h-int-main-void/alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ 
+int main(void) {
+	// your code goes here
+	char s;
+	scanf("%s",s);
+	if(s>=96||s<=122)
+	printf("alphabet");
+	else
+	printf("Not a alphabet");
+	return 0;
+}
